1ea98f3cac2c78e026c2aa0ba38392c05d8835b3
xiaosi <sam2020888@outlook.com>
2023-05-05 22:25:56 +0800

    feat: 初始化仓库

---
+++ .commitlintrc.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: sfy
+ * @Date: 2023-05-05 22:15:26
+ * @LastEditors: sfy
+ * @LastEditTime: 2023-05-05 22:15:27
+ * @FilePath: /big-react/.commitlintrc.js
+ * @Description: update here
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"]
+}; 
